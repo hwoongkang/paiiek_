ced1a70a62ba1241cfd01b5febbcb9bcfe415a0a
hyunwoong kang <hwoongkang@snu.ac.kr>
2020-01-07 01:53:34 +0900

    image now showing

--- client/src/components/pages/Design.js
+++ client/src/components/pages/Design.js
@@ -1,31 +1,37 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+
+//import test from './oneAndAHalf_physical_1.jpeg'
 
 const Design = () => {
-    const [designs,setDesigns] = useState([]);
+  const [designs, setDesigns] = useState([]);
 
-    useEffect(() => {
-        fetch("/api/designs")
-        .then(res => res.json())
-        .then(fetched=>setDesigns(fetched))
-        console.log("Design mounted")
-    }, [])
-    
+  useEffect(() => {
+    fetch("/api/designs")
+      .then(res => res.json())
+      .then(fetched => setDesigns(fetched));
+    console.log("Design mounted");
+  }, []);
 
-    return (
-        <React.Fragment>
-            <h1>Design</h1>
-            <p>
-                Designs including albums, posters, etc. will be listed here.
-            </p>
-            <p>
-                {designs.map(design => 
-                    <li key = {design.id}>
-                        {design.figureName}: {design.projectName}
-                    </li>
-                )}
-            </p>
-        </React.Fragment>
-    );
-}
+  return (
+    <div>
+      <h1>Design</h1>
+      <p>Designs including albums, posters, etc. will be listed here.</p>
+      <p>
+        {designs.map(design => (
+          <li key={design.id}>
+            {design.figureName}: {design.projectName}
+          </li>
+        ))}
+      </p>
+      <p>
+        <img
+          src="/img/oneAndAHalf_physical_1.jpeg"
+          className="test"
+          alt="test failed"
+        />
+      </p>
+    </div>
+  );
+};
 
 export default Design;
